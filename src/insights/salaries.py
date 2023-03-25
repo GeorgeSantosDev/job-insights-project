0b9c1fe9bf936a2df8d98bdfe9723e8ef4e4fbd1
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-03-24 21:11:41 -0300

    feat: Create matches_salary_range function

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -4,8 +4,9 @@ from src.insights.jobs import read
 
 def get_max_salary(path: str) -> int:
     jobs_data = read(path)
-    salaries = [job['max_salary'] for job in jobs_data
-                if job['max_salary'].isdigit()]
+    salaries = [
+        job['max_salary'] for job in jobs_data if job['max_salary'].isdigit()
+    ]
     int_salaries = list(map(int, salaries))
 
     return max(int_salaries)
@@ -13,44 +14,67 @@ def get_max_salary(path: str) -> int:
 
 def get_min_salary(path: str) -> int:
     jobs_data = read(path)
-    salaries = [int(job['min_salary']) for job in jobs_data
-                if job['min_salary'].isdigit()]
+    salaries = [
+        int(job['min_salary'])
+        for job in jobs_data
+        if job['min_salary'].isdigit()
+    ]
     int_salaries = list(map(int, salaries))
 
     return min(int_salaries)
 
 
+def check_salary(salary: Union[int, str]) -> bool:
+    try:
+        int(salary)
+        return False
+    except ValueError:
+        return True
+    except TypeError:
+        return True
+
+
+def check_min_max(min: Union[int, str], max: Union[int, str]) -> bool:
+    try:
+        return int(min) > int(max)
+    except TypeError:
+        return True
+
+
+def check_fields_are_digits(
+    min: Union[int, str], max: Union[int, str]
+) -> bool:
+    try:
+        return not str(min).isnumeric() or not str(max).isnumeric()
+    except KeyError:
+        return True
+
+
+def check_if_fields_exist(job: Dict) -> bool:
+    return 'min_salary' not in job or 'max_salary' not in job
+
+
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
+    validate1 = check_if_fields_exist(job)
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
+    validate2 = check_fields_are_digits(
+        job.get('min_salary'), job.get('max_salary')
+    )
 
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
+    validate3 = check_min_max(job.get('min_salary'), job.get('max_salary'))
 
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+    validate4 = check_salary(salary)
+
+    if validate1 or validate2 or validate3 or validate4:
+        raise ValueError
+
+    return int(job['min_salary']) <= int(salary) <= int(job['max_salary'])
 
 
 def filter_by_salary_range(
-    jobs: List[dict],
-    salary: Union[str, int]
+    jobs: List[dict], salary: Union[str, int]
 ) -> List[Dict]:
-    """Filters a list of jobs by salary range
+    '''Filters a list of jobs by salary range
 
     Parameters
     ----------
@@ -63,5 +87,5 @@ def filter_by_salary_range(
     -------
     list
         Jobs whose salary range contains `salary`
-    """
+    '''
     raise NotImplementedError
