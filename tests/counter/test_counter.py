2d3714f7e499348e5eb5d34a15ad6506bc7c19f8
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2023-03-27 09:27:20 -0300

    test: Create test for counter function

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,19 @@
-# from src.pre_built.counter import count_ocurrences
+import pytest
+from src.pre_built.counter import count_ocurrences
+from unittest.mock import mock_open, patch
 
 
-def test_counter():
-    pass
+@pytest.fixture
+def csv_content():
+    return '''
+    job_title,company,state,city, industry,rating, id
+    developer, AB, MG, BH, Finance, 4.0, 0
+    developer, Ab, MG, BH, finance, 2.0, 1
+    developer, ABD, MG, BH, Finance, 3.0, 2
+    '''
+
+
+def test_counter(csv_content):
+    with patch("builtins.open", mock_open(read_data=csv_content)):
+        assert count_ocurrences('data/jobs.csv', 'developer') == 3
+        assert count_ocurrences('data/jobs.csv', 'Finance') == 3
